224e3011488c4789dc99eb7547c37d647373333c
louis <louismayor1@gmail.com>
2025-12-03 20:50:52 +0000

    tidied home.nix, added basic vim config

--- home.nix
+++ home.nix
@@ -8,60 +8,74 @@
     # add custom cli things here
     home.activation = {
         # https://github.com/adi1090x/rofi?tab=readme-ov-file
-	setup_rofi = lib.hm.dag.entryAfter ["writeBoundary"] ''
-		rofi_dir="$HOME/.config/rofi/"
-		if [ ! -d "$rofi_dir" ]; then
-			mkdir $rofi_dir
-		fi
+		setup_rofi = lib.hm.dag.entryAfter ["writeBoundary"] ''
+			rofi_dir="$HOME/.config/rofi/"
+			if [ ! -d "$rofi_dir" ]; then
+				mkdir $rofi_dir
+			fi
 
-		if [ -n "$(ls -A "$rofi_dir")" ]; then
-			cd $rofi_dir
-			git clone --depth=1 https://github.com/adi1090x/rofi.git
-		fi
-		'';
-	# todo - dotfiles
+			if [ -n "$(ls -A "$rofi_dir")" ]; then
+				cd $rofi_dir
+				git clone --depth=1 https://github.com/adi1090x/rofi.git
+			fi
+			'';
+		# todo - dotfiles
     };
 
-    programs.bash = {
-        enable = true;
-        shellAliases = {
-            btw = "echo hello from home.nix";
-            nrs = "sudo nixos-rebuild switch --flake $HOME/.config/nixos/";
-	    vim = "nvim";
-	    usrconf = "cd $HOME/.config/";
-	    nixconf = "cd $HOME/.config/nixos/";
-        };
-	# https://mynixos.com/home-manager/option/home.sessionVariables
-	sessionVariables = {
-            usrconf = "$HOME/.config/";
-	    nixconf = "$HOME/.config/nixos/";
-        };
-        profileExtra = ''
-            if [-z "$WAYLAND_DISPLAY"] && ["$XDG_VTNR" = 1]; then
-                exec hyprland
-	    fi
-        '';
+    programs = {
+    	bash = {
+       		enable = true;
+       	 	shellAliases = {
+       	 	    btw = "echo hello from home.nix";
+       	 	    nrs = "sudo nixos-rebuild switch --flake $HOME/.config/nixos/";
+       	 	    vim = "nvim";
+       	 	    usrconf = "cd $HOME/.config/";
+       	 	    nixconf = "cd $HOME/.config/nixos/";
+       	 	};
+       	 	# https://mynixos.com/home-manager/option/home.sessionVariables
+       	 	sessionVariables = {
+       	 	    usrconf = "$HOME/.config/";
+       	 	    nixconf = "$HOME/.config/nixos/";
+       	 	};
+       	 	profileExtra = ''
+       	 	    if [-z "$WAYLAND_DISPLAY"] && ["$XDG_VTNR" = 1]; then
+       	 	        exec hyprland
+       	 	    fi
+       	 	'';
+		};
+    	fzf = {
+    		enable = true;
+			enableBashIntegration = true;
+		};
+		firefox = {
+			enable = true;
+		};
+		alacritty = {
+			enable = true;
+		};
+		# already enabled system-wide
+		neovim = {
+			enable = true;
+			# basic config - would be overridden by dotfile config later
+			extraConfig = ''
+				set number relativenumber
+				set shiftwidth=4
+				set tabstop=4
+				set smarttab
+			'';
+		};
     };
 
-    # programs.fzf.enableBashIntegration = true;
-    programs.fzf = {
-    	enable = true;
-	# defaultCommand;
-    };
-
-    programs.firefox.enable = true;
-
-    programs.alacritty.enable = true;
-
+    
     home.packages = with pkgs; [
     	# cli utils
-	gh
-	bat
-	fzf
-	rofi
-	# fonts
-	nerd-fonts.caskaydia-cove
-	nerd-fonts.iosevka
-	font-awesome_6
+		gh
+		bat
+		fzf
+		rofi
+		# fonts
+		nerd-fonts.caskaydia-cove
+		nerd-fonts.iosevka
+		font-awesome_6
     ];
 }
